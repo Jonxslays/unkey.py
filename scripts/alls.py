f0aad4f310c2ece6ab909e20395d659f12631d3e
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-06-22 08:04:29 -0700

    Add scripts

---
+++ scripts/alls.py
@@ -0,0 +1,46 @@
+import sys
+import typing as t
+
+import unkey
+
+
+def should_include_module(module: str) -> bool:
+    return (
+        module != "annotations" and module[0] != "_" and module[0].upper() != module[0]
+    )
+
+
+def get_modules() -> t.List[str]:
+    return [m for m in unkey.__dict__ if should_include_module(m)]
+
+
+def get_alls() -> t.Tuple[t.Set[str], t.Set[str]]:
+    modules = get_modules()
+    return (
+        set(item for module in modules for item in unkey.__dict__[module].__all__),
+        set(i for i in unkey.__all__ if i not in modules),
+    )
+
+
+def validate_alls() -> None:
+    modules, lib = get_alls()
+    err = None
+
+    if missing := modules - lib:
+        err = "Missing exported items at top level:\n" + "\n".join(
+            f" - {m}" for m in missing
+        )
+        print(err, file=sys.stderr)
+
+    if missing := lib - modules:
+        err = "Missing exported items at module level:\n" + "\n".join(
+            f" - {m}" for m in missing
+        )
+        print(err, file=sys.stderr)
+
+    if err:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    validate_alls()
---
+++ scripts/version-check.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+PROJECT_VERS=$(grep -m 1 -oP 'version = "(.*)"' pyproject.toml | sed -rn 's/.*"(.*)"/v\1/p');
+INIT_VERS=$(sed -rn 's/__version__: Final\[str\] = "(.*)"/v\1/p' unkey/__init__.py);
+
+if [ ! "$INIT_VERS" = "$PROJECT_VERS" ]; then
+    echo "Project $PROJECT_VERS doesn't match init $INIT_VERS";
+    exit 1;
+fi
+
+echo $PROJECT_VERS;
