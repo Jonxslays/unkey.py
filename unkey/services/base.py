764438ef68606f7690afe63b0f913f2034444c24
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-06-23 20:32:07 -0700

    Add base service

--- unkey/services/base.py
+++ unkey/services/base.py
@@ -1,9 +1,29 @@
 from __future__ import annotations
 
 import abc
+import typing as t
+
+from unkey import serializer
+from . import HttpService
 
 __all__ = ("BaseService",)
 
 
 class BaseService(abc.ABC):
-    ...
+    """The base service all API services inherit from.
+
+    Args:
+        http_service: The http service to use for requests.
+
+        serializer: The serializer to use for handling incoming
+            JSON data from the API.
+    """
+
+    __slots__ = ("_http", "_serializer")
+
+    def __init__(self, http_service: HttpService, serializer: serializer.Serializer) -> None:
+        self._http = http_service
+        self._serializer = serializer
+
+    def _generate_map(self, **kwargs: t.Any) -> t.Dict[str, t.Any]:
+        return {k: v for k, v in kwargs.items() if v is not None}
