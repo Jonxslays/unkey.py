35cc16881add43b347efead851e326fb896e46d2
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-07-24 15:51:37 -0700

    Add http service tests

---
+++ tests/services/test_http.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import pytest
+
+from unkey import HttpService
+from unkey import constants
+
+
+def test_init() -> None:
+    http = HttpService("abc123", None, None)
+
+    assert http._ok_responses == {200, 202}  # type: ignore
+    assert http._api_version == "/v1"  # type: ignore
+    assert http._base_url == constants.API_BASE_URL  # type: ignore
+    assert http._headers == {  # type: ignore
+        "x-user-agent": constants.USER_AGENT,
+        "Authorization": "Bearer abc123",
+    }
+
+
+def test_init_fails_without_token() -> None:
+    with pytest.raises(ValueError) as e:
+        HttpService("", None, None)
+
+    assert e.exconly() == "ValueError: Api key must be provided."
+
+
+def test_full_init() -> None:
+    http = HttpService("abc123", 4, "1234")
+
+    assert http._ok_responses == {200, 202}  # type: ignore
+    assert http._api_version == "/v4"  # type: ignore
+    assert http._base_url == "1234"  # type: ignore
+    assert http._headers == {  # type: ignore
+        "x-user-agent": constants.USER_AGENT,
+        "Authorization": "Bearer abc123",
+    }
