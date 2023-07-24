c37efb65cbf213ea02309a88d8d78b2422b851b4
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-07-24 14:36:56 -0700

    Finish base service tests

--- tests/services/test_base_service.py
+++ tests/services/test_base_service.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from datetime import datetime
 from unittest import mock
 
 import pytest
@@ -29,3 +30,49 @@ def test_generate_map_with_none(service: BaseService) -> None:
     result = service._generate_map(one=1, two=None)  # type: ignore
 
     assert result == {"one": 1, "two": None}
+
+
+def test_expires_in_returns_undefined(service: BaseService) -> None:
+    result = service._expires_in()  # type: ignore
+
+    assert result is UNDEFINED
+
+
+@mock.patch("unkey.services.base.datetime", wraps=datetime)
+def test_expires_in_milliseconds(dt: mock.Mock, service: BaseService) -> None:
+    dt.now = mock.Mock(return_value=datetime(2000, 1, 1, 1, 1, 1))
+
+    result = service._expires_in(milliseconds=5000)  # type: ignore
+
+    assert result
+    assert datetime.fromtimestamp(result // 1000) == datetime(2000, 1, 1, 1, 1, 6)
+
+
+@mock.patch("unkey.services.base.datetime", wraps=datetime)
+def test_expires_in_seconds(dt: mock.Mock, service: BaseService) -> None:
+    dt.now = mock.Mock(return_value=datetime(2000, 1, 1, 1, 1, 1))
+
+    result = service._expires_in(seconds=3)  # type: ignore
+
+    assert result
+    assert datetime.fromtimestamp(result // 1000) == datetime(2000, 1, 1, 1, 1, 4)
+
+
+@mock.patch("unkey.services.base.datetime", wraps=datetime)
+def test_expires_in_minutes(dt: mock.Mock, service: BaseService) -> None:
+    dt.now = mock.Mock(return_value=datetime(2000, 1, 1, 1, 1, 1))
+
+    result = service._expires_in(minutes=2)  # type: ignore
+
+    assert result
+    assert datetime.fromtimestamp(result // 1000) == datetime(2000, 1, 1, 1, 3, 1)
+
+
+@mock.patch("unkey.services.base.datetime", wraps=datetime)
+def test_expires_in_days(dt: mock.Mock, service: BaseService) -> None:
+    dt.now = mock.Mock(return_value=datetime(2000, 1, 1, 1, 1, 1))
+
+    result = service._expires_in(days=6)  # type: ignore
+
+    assert result
+    assert datetime.fromtimestamp(result // 1000) == datetime(2000, 1, 7, 1, 1, 1)
